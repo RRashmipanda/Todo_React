519cbc55e9450decf9086a4eebe196402032f403
Rashmi Panjan Panda <140526128+RRashmipanda@users.noreply.github.com>
2023-09-23 08:35:41 +0530

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,15 @@
+import './globals.css'
+
+
+//rootlayout server part
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      
+       
+       <body >
+        
+        {children}</body>
+    </html>
+  )
+}
